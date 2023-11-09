3b0b338555edecf3ee2a6465c176e537137ba446
BrunoRozas <brunorozas@gmail.com>
2023-11-09 16:07:53 -0300

    tarjetas
    tarjetas agregadas, responsivas

---
+++ src/atoms/ButtonReserve.js
@@ -0,0 +1,35 @@
+const ButtonReserve = ({bgcolor}) => {
+
+
+
+    return (
+      <>
+        <button>Reservar ahora</button>
+        <style jsx>
+          {`
+            button {
+              color: white;
+              background-color: ${bgcolor ? bgcolor : "gray"};
+              border: none;
+              border-radius: 0.5rem;
+              box-shadow: 1px 2px 2px black;
+              padding: 10px 20px;
+              text-transform: uppercase;
+              font-weight: bold;
+              transition: 0.3s;
+              margin:auto;
+              cursor: pointer;
+              
+            }
+            button:hover {
+              transform: traslateY(2px);
+              box-shadow: none;
+  
+            }
+          `}
+        </style>
+      </>
+    );
+  };
+  
+  export default ButtonReserve;
