c2a8e95466efec932d8071e1637a8b0cf083da2d
BrunoRozas <brunorozas@gmail.com>
2023-11-10 16:10:11 -0300

    paleta de colores
    paleta de colores agregada como variables al root de los estilos globales

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import CardsSection from '@/organims/CardsSection'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function Home() {
       </Head>
       <main>
         holaaaa ok
+        <CardsSection />
       </main>
     </>
   )
