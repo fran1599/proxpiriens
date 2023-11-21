6ac39c5c69a4df5a54131321be279a00a77cc1e5
paula <paulabboca@hotmail.com>
2023-11-20 21:43:41 -0300

    Footer
    Agrego dentro de components hoja footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <div className="banner">
+          <img src="" alt="Banner" />
+        </div>
+        <div className="contact">
+          <h2>Contacto</h2>
+          <p>0800-999-0000</p>
+          <p>info@proxpirienstravel.com</p>
+        </div>
+        <div className="additional-info">
+          <h2>Buscanos en Redes</h2>
+          <p>Chateá con un asesor</p>
+        </div>
+      </footer>
+    );
+  };
+  
