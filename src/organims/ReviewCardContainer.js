6c4161b802b386294e8ba0f2c5ee5a0988d8af2c
pamfreytes <pamfreytes@gmail.com>
2023-11-27 12:56:08 -0300

    Reviews Container
    Incorporación contenedor de reviews

---
+++ src/organims/ReviewCardContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReviewCard from './ReviewCard';
+
+const reviews = [
+  { author: 'Laura S.', comment: '"Con una amiga compramos el paquete de 6 días a las Cataratas del Iguazú. Es súper recomendable y lo volvería a repetir"', rating: 5, imageUrl:'./img/images/Laura-profile.jpg'},
+  { author: 'Andy B.', comment: '"Fuimos a avistar ballenas a Puerto Madryn con mi esposa. Fue mágico"', rating: 5, imageUrl: './img/images/Andy-profile.webp' },
+  { author: 'Nora G.', comment: '"Pensaba que las excursiones no eran para mí, pero pude encontrarme con la naturaleza y disfrutar de principio a fin"', rating: 5, imageUrl: './img/images/Nora-profile.webp'},
+  { author: 'Vivian W.', comment: '"Excelente en todos los aspectos. Ya estoy pensando cuál va a ser el próximo destino que voy a conocer"', rating: 5, imageUrl: './img/images/Vivian-profile.webp'},
+  {author: 'Jorge D', comment: '"En mis próximas vacaciones ya sé con qué empresa voy a viajar @proxpirienstravel"', rating: 5, imageUrl: './img/images/Jorge-profile.jpeg' }
+
+];
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Las mejores Experiencias</h1>
+      <div className="review-container">
+        {reviews.map((review, index) => (
+          <ReviewCard key={index} review={review} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ReviewCardContainer;
