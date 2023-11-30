cf35013e4d67ffd41608134fa1c92635b4f556d7
Leandro Nahuel <leandro.nahuel7@gmail.com>
2023-11-30 02:00:50 -0300

    estructura del footer

--- src/organims/footer.js
+++ src/organims/footer.js
@@ -5,20 +5,71 @@ const Footer = () => {
     return (
       <>
       <footer className="footer">
-        <div className="banner">
-          <img src="falta agregar ruta img" alt="Banner" />
-        </div>
-        <div className="contact">
-          <h2>Contacto</h2>
-          <p>0800-999-0000</p>
-          <p>info@proxpirienstravel.com</p>
-          <p>Buscanos en Redes</p>
-          <p>Chateá con un asesor</p>
-          <p>Buscanos en Redes</p>
-          <p>Chateá con un asesor</p>
-        </div>
+
+
         
-      </footer>
+<div className="banner">
+<a href="https://www.facebook.com/"><img src="./img/footer/facebook.svg" alt="Facebook" className="icons-banner" /></a>
+<a href="https://www.instagram.com/"><img src="./img/footer/instagram.svg" alt="Instagram" className="icons-banner" /></a>
+<a href="https://www.twitter.com/"><img src="./img/footer/twitter.svg" alt="Twitter" className="icons-banner-fullscreen" /></a>
+<a href="https://web.whatsapp.com/"><img src="./img/footer/whatsapp.svg" alt="Whatsapp" className="icons-banner" /></a>
+<a href="https://tiktok.com/"><img src="./img/footer/tiktok.svg" alt="TikTok" className="icons-banner-fullscreen" /></a>
+<a href="https://web.telegram.org/a/"><img src="./img/footer/telegram.svg" alt="Telegram" className="icons-banner-fullscreen" /></a>
+</div>
+
+
+
+<div className="footer-links">
+  
+    <img src="./LogoColorFloating.png" alt="Proxpiriens" />
+ 
+
+    <div className="oferta-turistica">
+      <h2>Oferta Turística</h2>
+      <p><a href="">San Carlos de Bariloche</a></p>
+      <p><a href="">Ciudad de Buenos Aires</a></p>
+      <p><a href="">Cataratas del Iguazú</a></p>
+      <p><a href="">Cerro de Los 7 Colores</a></p>
+      <p><a href="">Ciudad de Córdoba</a></p>
+      <p><a href="">Gualeguaychú</a></p>
+      <p><a href="">Mendoza</a></p>
+      <p><a href="">Glaciar Perito Moreno</a></p>
+      <p><a href="">Puerto Madryn</a></p>
+      <p><a href="">Ushuaia</a></p>
+      <p><a href="">Valle de La Luna</a></p>
+    </div>
+
+    <div className="categorias">
+      <h2>Categorías</h2>
+      <p><a href="">Estadía</a></p>
+      <p><a href="">Traslado</a></p>
+      <p><a href="">Excursiones</a></p>
+      <p><a href="">Paquetes</a></p>
+    </div>
+
+    <div className="club-de-beneficios">
+      <h2>Club de Beneficios</h2>
+      <p><a href="">Inscribirme</a></p>
+    </div>
+
+    <div className="nosotros">
+      <h2>Nosotros</h2>
+      <p><a href="">Nuestra Historia</a></p>
+      <p><a href="">Trabajá con Nosotros</a></p>
+      <p><a href="">Agencias Afiliadas</a></p>
+      <p><a href="">Preuntas Frecuentes</a></p>
+    </div>
+
+    <div className="contacto">
+      <h2>Contacto</h2>
+      <p>0800-999-0000</p>
+      <p>info@proxpirienstravel.com</p>
+      <p><a href="">Buscanos en Redes</a></p>
+      <p><a href="">Chateá con un asesor</a></p>
+    </div>
+  
+</div>
+</footer>
       </>
     );
   };
