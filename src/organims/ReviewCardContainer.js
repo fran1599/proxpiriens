42d29e6a82846c471fa0d9ba89453816f5454a4d
pamfreytes <pamfreytes@gmail.com>
2023-12-01 17:39:39 -0300

    Correcciones
    Modificaciones de rutas de imágenes

--- src/organims/ReviewCardContainer.js
+++ src/organims/ReviewCardContainer.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../styles/Home.module.css';
 import ReviewCard from './ReviewCard';
 
 const reviews = [
-  { author: 'Laura S.', comment: '"Con una amiga compramos el paquete de 6 días a las Cataratas del Iguazú. Es súper recomendable y lo volvería a repetir"', rating: 5, imageUrl:'../../public/img/profiles/Laura-profile.jpg'},
+  { author: 'Laura S.', comment: '"Con una amiga compramos el paquete de 6 días a las Cataratas del Iguazú. Es súper recomendable y lo volvería a repetir"', rating: 5, imageUrl:'/img/profiles/Laura-profile.jpg'},
   { author: 'Andy B.', comment: '"Fuimos a avistar ballenas a Puerto Madryn con mi esposa. Fue mágico"', rating: 5, imageUrl: '../../public/img/profiles/Andy-profile.webp' },
-  { author: 'Nora G.', comment: '"Pensaba que las excursiones no eran para mí, pero pude encontrarme con la naturaleza y disfrutar de principio a fin"', rating: 5, imageUrl: '../../public/img/profiles/Nora-profile.webp'},
-  { author: 'Vivian W.', comment: '"Excelente en todos los aspectos. Ya estoy pensando cuál va a ser el próximo destino que voy a conocer"', rating: 5, imageUrl: '../../public/img/profiles/Vivian-profile.webp'},
-  {author: 'Jorge D', comment: '"En mis próximas vacaciones ya sé con qué empresa voy a viajar @proxpirienstravel"', rating: 5, imageUrl: '../../public/img/profiles/Jorge-profile.jpeg' }
+  { author: 'Nora G.', comment: '"Pensaba que las excursiones no eran para mí, pero pude encontrarme con la naturaleza y disfrutar de principio a fin"', rating: 5, imageUrl: '/img/profiles/Nora-profile.webp'},
+  { author: 'Vivian W.', comment: '"Excelente en todos los aspectos. Ya estoy pensando cuál va a ser el próximo destino que voy a conocer"', rating: 5, imageUrl: '/img/profiles/Vivian-profile.webp'},
+  {author: 'Jorge D', comment: '"En mis próximas vacaciones ya sé con qué empresa voy a viajar @proxpirienstravel"', rating: 5, imageUrl: '/img/profiles/Jorge-profile.jpeg' }
 
 ];
 
-const App = () => {
+const ReviewCardContainer = () => {
   return (
     <div className="app">
       <h1>Las mejores Experiencias</h1>
